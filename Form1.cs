3b871f967307d5d887d7d105ef8312d0a3c4b1f5
mahloola <mahloola08@gmail.com>
2021-01-13 20:49:57 -0500

    everything compiles now memory reader has been added, which prints out the song name

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OsuMemoryDataProvider;
 
 namespace bmviewer
 {
     public partial class Form1 : Form
     {
+        string directory = @"E:\osu!\Songs";
+        OsuMemoryReader memoryReader = new OsuMemoryReader();
         public Form1()
         {
             InitializeComponent();
+            timer1.Start();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string folderName = memoryReader.GetMapFolderName();
+            Console.WriteLine(folderName);
+            string fileName = memoryReader.GetOsuFileName();
+            Console.WriteLine(fileName);
+
+        }
+
     }
 }
